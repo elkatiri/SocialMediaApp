f844eb4f25269f40ecc75ab22d68c519dc76c489
ahmed elkatiri <helloahmed2002@gmail.com>
2026-04-15 12:55:02 +0100

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text,View} from 'react-native'
+
+
+export const index = () => {
+  return (
+    <View>
+      <Text>index here </Text>
+    </View>
+  )
+} 
+export default index
